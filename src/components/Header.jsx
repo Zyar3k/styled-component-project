b10329f1c1d9b8d26033d28ee3355826c34b831f
Arkadiusz Zygan <a.zygan@gmail.com>
2021-09-21 22:15:55 +0200

    Edit header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
-import { StyledHeader } from "./styles/Header.styled";
+import { StyledHeader, Nav, Logo } from "./styles/Header.styled";
+import { Container } from "./styles/Container.styled";
+import { Button } from "./styles/Button.styled";
 
 const Header = () => {
   return (
     <StyledHeader>
-      <h1>Bubble</h1>
+      <Container>
+        <Nav>
+          <Logo src='./images/logo.svg' alt='logo' />
+          <Button>Try it fo free</Button>
+        </Nav>
+      </Container>
     </StyledHeader>
   );
 };
